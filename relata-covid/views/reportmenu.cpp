d72f199054b3011a3b7314d58fbb847fbfceb2a4
Matheus Almeida <tthheusalmeida@gmail.com>
2020-10-11 18:54:29 -0300

    feat(#20): Add functionality to the buttons of the ReportMenu GUI

--- relata-covid/views/reportmenu.cpp
+++ relata-covid/views/reportmenu.cpp
@@ -12,3 +12,78 @@ ReportMenu::~ReportMenu()
 {
     delete ui;
 }
+
+void ReportMenu::on_backButton_pressed()
+{
+    this->reject();
+}
+
+void ReportMenu::on_esusVEButton_pressed()
+{
+    try{
+        QString file_path = QFileDialog::getOpenFileName(this, "Open a file", QDir::homePath());
+
+        string path = file_path.toStdString();
+
+        bool isCSV = (path.substr(path.size() - 4, path.size())) == ".csv" ? true : false;
+
+        if(!isCSV){
+            QMessageBox::information(this, tr("Relatório"), tr("O arquivo selecionado não é do tipo CSV"));
+            path = "";
+        }
+        this->e_susVE_path = path;
+    } catch (exception e) { }
+}
+
+string ReportMenu::getSivep_DDA_path() const
+{
+    return sivep_DDA_path;
+}
+
+void ReportMenu::setSivep_DDA_path(const string &value)
+{
+    sivep_DDA_path = value;
+}
+
+string ReportMenu::getE_susVE_path() const
+{
+    return e_susVE_path;
+}
+
+void ReportMenu::setE_susVE_path(const string &value)
+{
+    e_susVE_path = value;
+}
+
+void ReportMenu::on_sivepDDAButton_pressed()
+{
+    try{
+        QString file_path = QFileDialog::getOpenFileName(this, "Open a file", QDir::homePath());
+
+        string path = file_path.toStdString();
+
+        bool isCSV = (path.substr(path.size() - 4, path.size())) == ".csv" ? true : false;
+
+        if(!isCSV){
+            QMessageBox::information(this, tr("Relatório"), tr("O arquivo selecionado não é do tipo CSV"));
+            path = "";
+        }
+        this->sivep_DDA_path = path;
+    } catch (exception e) { }
+}
+
+void ReportMenu::on_generateButton_pressed()
+{
+    if(this->e_susVE_path.length() != 0 && this->sivep_DDA_path.length() != 0){
+        //call the next window
+    }
+    else if(this->e_susVE_path.length() != 0 && this->sivep_DDA_path.length() == 0){
+        QMessageBox::information(this, tr("Relatório"), tr("O arquivo do SIVEP-DDA não foi importado."));
+    }
+    else if(this->e_susVE_path.length() == 0 && this->sivep_DDA_path.length() != 0){
+        QMessageBox::information(this, tr("Relatório"), tr("O arquivo do e-SUS não foi importado."));
+    }
+    else {
+        QMessageBox::information(this, tr("Relatório"), tr("Arquivos não foram importados."));
+    }
+}
--- relata-covid/views/reportmenu.h
+++ relata-covid/views/reportmenu.h
@@ -2,6 +2,12 @@
 #define REPORTMENU_H
 
 #include <QDialog>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QDir>
+#include <QtGui>
+
+using namespace std;
 
 namespace Ui {
 class ReportMenu;
@@ -15,8 +21,25 @@ public:
     explicit ReportMenu(QWidget *parent = nullptr);
     ~ReportMenu();
 
+    string getE_susVE_path() const;
+    void setE_susVE_path(const string &value);
+
+    string getSivep_DDA_path() const;
+    void setSivep_DDA_path(const string &value);
+
+private slots:
+    void on_backButton_pressed();
+
+    void on_esusVEButton_pressed();
+
+    void on_sivepDDAButton_pressed();
+
+    void on_generateButton_pressed();
+
 private:
     Ui::ReportMenu *ui;
+    string e_susVE_path;
+    string sivep_DDA_path;
 };
 
 #endif // REPORTMENU_H
