60459caab130fae508ce532e36cf8100c8763c09
costascaio <caio.soares@aluno.ufop.edu.br>
2020-10-11 16:51:50 -0300

    reverting a commit

--- relata-covid/src/services/generate_duplicity_report.cpp
+++ relata-covid/src/services/generate_duplicity_report.cpp
@@ -13,28 +13,21 @@ int find(vector<Person*> people, Person* person_duplicity, int it){
     return -1;
 }
 
-Report* generate_report(vector<Person*> people_SIVEP, vector<Person*> people_ESUS){
+Report* generate_report(vector<Person*> people){
     int it, position;
     Report* report = new ReportImpl();
-    vector<Person*> people_without_duplicity;
-
     int duplicity = 0;
 
-    for (int i = 0; i < people_SIVEP.size(); i++, it++) {
-        position = find(people_ESUS, people_SIVEP[i], it);
-        people_without_duplicity.push_back(people_SIVEP[i]);
+    for (int i = 0; i < people.size(); i++) {
+        position = find(people, people[i], it);
         if(position != -1){
-            people_ESUS.erase(people_ESUS.begin() + position);
-            duplicity++;
+            people.erase(people.begin() + position);
         }
-    }
-
-    for (int i = 0; i < people_ESUS.size(); i++) {
-        people_without_duplicity.push_back(people_ESUS[i]);
+        it++;
     }
 
     report->setDuplicity(duplicity);
-    report->setPeople(people_without_duplicity);
+    report->setPeople(people);
 
     return report;
 }
