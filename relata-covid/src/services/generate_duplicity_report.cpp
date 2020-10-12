d9742a85ff412e9ce915e40ab9006bb46002f382
costascaio <caio.soares@aluno.ufop.edu.br>
2020-10-11 21:06:21 -0300

    bug(#24): fix bug from generate duplicity report

--- relata-covid/src/services/generate_duplicity_report.cpp
+++ relata-covid/src/services/generate_duplicity_report.cpp
@@ -1,33 +1,37 @@
 #include "generate_duplicity_report.h"
 
-int find(vector<Person*> people, Person* person_duplicity, int it){
-    int position = 0;
-    for (int i = it + 1; i < people.size(); i++){
-        position++;
+int find(vector<Person*>& people, Person* person_duplicity){
+    for (int i = 0; i < people.size(); i++){
         if(people[i]->getCPF() == person_duplicity->getCPF()){
-
-            return position;
+            return i;
         }
     }
 
     return -1;
 }
 
-Report* generate_report(vector<Person*> people){
-    int it, position;
+Report* generate_report(vector<Person*>& people_SIVEP, vector<Person*>& people_ESUS){
+    int position;
     Report* report = new ReportImpl();
+    vector<Person*> people_without_duplicity;
     int duplicity = 0;
 
-    for (int i = 0; i < people.size(); i++) {
-        position = find(people, people[i], it);
+    cout << "sivep size: " << people_SIVEP.size() << endl;
+    for (int i = 0; i < people_SIVEP.size(); i++) {
+        position = find(people_ESUS, people_SIVEP[i]);
+        people_without_duplicity.push_back(people_SIVEP[i]);
         if(position != -1){
-            people.erase(people.begin() + position);
+            people_ESUS.erase(people_ESUS.begin() + position);
+            duplicity++;
         }
-        it++;
     }
 
+    for (int i = 0; i < people_ESUS.size(); i++) {
+          people_without_duplicity.push_back(people_ESUS[i]);
+      }
+
+    report->setPeople(people_without_duplicity);
     report->setDuplicity(duplicity);
-    report->setPeople(people);
 
     return report;
 }
